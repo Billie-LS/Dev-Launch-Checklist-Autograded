5b4fe9a9feb8f7db28d73cda7241dfbe129eb61e
Billie-LS <lskylizard@yahoo.com>
2024-03-07 21:27:49 -0600

    faulty helper, form submission

--- scriptHelper.js
+++ scriptHelper.js
@@ -72,36 +72,34 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     "copilotStatus"
   ).innerHTML = `Co-pilot ${copilot} is ready for launch`;
 
-  if (fuelLevel < 10000) {
-    document.getElementById("faultyItems").style.visibility = "visible";
-    document.getElementById("fuelStatus").innerHTML =
-      "Fuel level too low for launch";
-    document.getElementById("launchStatus").innerHTML =
-      "Shuttle Not Ready for Launch";
-    document.getElementById("launchStatus").style.color = "red";
-  } else {
-    document.getElementById("fuelStatus").innerHTML =
-      "Fuel level high enough for launch";
-  }
+  const faultyItems = document.getElementById("faultyItems");
+  const launchStatus = document.getElementById("launchStatus");
 
-  if (cargoLevel > 10000) {
-    document.getElementById("faultyItems").style.visibility = "visible";
-    document.getElementById("cargoStatus").innerHTML =
-      "Cargo mass too heavy for launch";
-    document.getElementById("launchStatus").innerHTML =
-      "Shuttle Not Ready for Launch";
-    document.getElementById("launchStatus").style.color = "red";
-  } else {
-    document.getElementById("cargoStatus").innerHTML =
-      "Cargo mass low enough for launch";
-  }
+  if (fuelLevel < 10000 || cargoLevel > 10000) {
+    faultyItems.style.visibility = "visible";
+    launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+    launchStatus.style.color = "red";
 
-  if (fuelLevel >= 10000 && cargoLevel <= 10000) {
+    if (fuelLevel < 10000) {
+      document.getElementById("fuelStatus").innerHTML =
+        "Fuel level too low for launch";
+    } else {
+      document.getElementById("fuelStatus").innerHTML =
+        "Fuel level high enough for launch";
+    }
+
+    if (cargoLevel > 10000) {
+      document.getElementById("cargoStatus").innerHTML =
+        "Cargo mass too heavy for launch";
+    } else {
+      document.getElementById("cargoStatus").innerHTML =
+        "Cargo mass low enough for launch";
+    }
+  } else {
     // Hide the faultyItems div when everything is good to go
-    document.getElementById("faultyItems").style.visibility = "hidden";
-    document.getElementById("launchStatus").innerHTML =
-      "Shuttle is Ready for Launch";
-    document.getElementById("launchStatus").style.color = "green";
+    faultyItems.style.visibility = "hidden";
+    launchStatus.innerHTML = "Shuttle is Ready for Launch";
+    launchStatus.style.color = "green";
   }
 }
 
